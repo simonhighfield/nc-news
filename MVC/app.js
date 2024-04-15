8dfe53b242e2243f08bd04c436ac5f244ee2a88d
Simon Highfield <simonhighfield@Simons-BigBook.local>
2024-04-15 16:06:23 +0100

    tiedied up comments

--- MVC/app.js
+++ MVC/app.js
@@ -5,7 +5,7 @@ const app = express()
 
 app.get('/api/topics', getTopics)
 
-// Return error for invalid endpoints
+// Return error for all requests to invalid endpoints i.e. get/invalidEndpoint
 app.all('*', (req, res, next) => {
     res.status(404).send({msg: 'endpoint not found'})   
         // question for whoever's giving me feedback: where is 'res' defined as either res or response?
